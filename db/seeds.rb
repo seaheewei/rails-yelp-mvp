6c8c146ab3a55c24d9519e1e4592874a15803322
Eewei Seah <seaheewei@gmail.com>
2023-02-13 20:28:40 +0800

    generated all the models and added validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample(),
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone
+  )
+end
